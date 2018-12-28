0ef0a2c082ad8fa9bb97fedade5023cfd2903567
Marcel Coelho <marcel.coelho@coopersystem.com.br>
2018-12-28 16:33:35 -0200

    refactor: remove estado do componente e trata-o como um container

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,11 @@
-import React, { Component } from 'react';
-import firebase from './services/firebase';
+import React from 'react';
+import { Container } from 'reactstrap';
 import List from './List';
 
-export default class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      count: -1,
-      error: null,
-      loading: true,
-    };
-
-    this.fetchData = this.fetchData.bind(this);
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  componentWillUnmount() {
-    this.setState = () => {};
-  }
-
-  async fetchData() {
-    try {
-      const countDoc = await firebase
-        .firestore()
-        .collection('messages')
-        .doc('count')
-        .get();
-
-      this.setState({
-        count: countDoc.exists ? countDoc.data().count : 0,
-        error: null,
-        loading: false,
-      });
-    } catch (error) {
-      this.setState({
-        count: -1,
-        error,
-        loading: false,
-      });
-    }
-  }
-
-  render() {
-    const { loading, count, error } = this.state;
-
-    if (loading) {
-      return 'Loading...';
-    }
-
-    if (error) {
-      return 'Error...';
-    }
-
-    return <List count={count} />;
-  }
+export default function App() {
+  return (
+    <Container>
+      <List />
+    </Container>
+  );
 }
