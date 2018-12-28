fee2bf5cae5dd22e5861699ff9e113d56a643047
husscode <flasd2@gmail.com>
2018-12-28 10:18:53 -0200

    fix: corrige problema ao verificar se contador existe

--- functions/src/controllers/aggregator.js
+++ functions/src/controllers/aggregator.js
@@ -1,11 +1,12 @@
 const firestore = require('../repository');
 
-module.exports = async function messageCountAggregator(event) {
+module.exports = async function messageCountAggregator(change) {
   const {
-    data: { exists, previous },
-  } = event;
+    before: { exists: existsBefore },
+    after: { exists: existsAfter },
+  } = change;
 
-  if (previous && exists) {
+  if (existsBefore && existsAfter) {
     return;
   }
 
@@ -18,9 +19,9 @@ module.exports = async function messageCountAggregator(event) {
 
     const count = countDoc.exists ? countDoc.data().count : 0;
 
-    if (!previous) {
+    if (!existsBefore) {
       countRef.set({ count: count + 1 });
-    } else if (!exists) {
+    } else if (!existsAfter) {
       countRef.set({ count: count - 1 });
     }
   } catch (error) {
